b5b05c7e56faa7ffb18d97ab36500baa429b9189
Rob Crowell <crowell@alum.mit.edu>
2011-01-22 08:01:22 -0500

    Initial commit.

---
+++ hithertunes.py
@@ -0,0 +1,95 @@
+import cherrypy
+import os
+import pyItunes as itunes
+import re
+import sqlite3
+import urllib
+
+from mako import exceptions
+from mako.lookup import TemplateLookup
+from mako.template import Template
+
+
+def absolute_path(path):
+    return os.path.abspath(os.path.expanduser(path))
+
+def decode_unicode_references(data):
+    def _callback(matches):
+        id = matches.group(1)
+        try:
+            return unichr(int(id))
+        except:
+            return id
+    return re.sub("&#(\d+)(;|(?=\s))", _callback, data)
+
+class iTunesExporter(object):
+    def __init__(self, database_location, templates_location, library_location, rebuild_library=False):
+        self.database_location = absolute_path(database_location)
+        self.template_lookup = TemplateLookup(directories=[templates_location])
+
+        if rebuild_library:
+            print "loading itunes:     %s" % (absolute_path(library_location),)
+            parser = itunes.XMLLibraryParser(absolute_path(library_location))
+            library = itunes.Library(parser.dictionary)
+
+            print "building sqlite3:   %s" % (self.database_location)
+            cursor = self._sql_cursor()
+            self._create_schema(cursor)
+            self._parse_library(library, cursor)
+            cursor.connection.commit()
+            cursor.connection.close()
+
+    def _sql_cursor(self):
+        connection = sqlite3.connect(self.database_location)
+        connection.row_factory = sqlite3.Row
+        return connection.cursor()
+
+    def _create_schema(self, cursor):
+        cursor.execute('''DROP TABLE songs''')
+        try: cursor.execute('''CREATE TABLE songs (id INTEGER PRIMARY KEY, name TEXT, artist TEXT, album TEXT, track_number INTEGER, year INTEGER, location TEXT)''')
+        except sqlite3.OperationalError, e: 
+            print e   # don't go any further here; db exists already
+            return
+
+    def _parse_library(self, library, cursor):
+        for song in library.songs:
+            if not song.artist or not song.album or not song.name: continue
+
+            cursor.execute('''INSERT INTO songs (artist, album, name, track_number, year, location) VALUES (?, ?, ?, ?, ?, ?)''', (song.artist, song.album, song.name, song.track_number, song.year, song.location))
+
+    @cherrypy.expose
+    def index(self):
+        """Display the whole library on one screen."""
+        cursor = self._sql_cursor()
+        cursor.execute('''SELECT id, artist, album, name, track_number, year FROM songs ORDER BY LOWER(artist), COALESCE(year, 99999), LOWER(album), COALESCE(track_number, 99999), LOWER(name), id''')
+        results = [x for x in cursor]
+        template = self.template_lookup.get_template('index.mak')
+        try: return template.render(songs=results)
+        except: return exceptions.html_error_template().render()
+
+        cursor.connection.close()
+
+    @cherrypy.expose
+    def song(self, song_id):
+        """Stream the bits of this song to the client."""
+        cursor = self._sql_cursor()
+        for row in cursor.execute('''SELECT location FROM songs WHERE id = ?''', (song_id,)):
+            cherrypy.response.headers['Content-type'] = 'audio/mpeg'
+            print row['location']
+            return urllib.urlopen(decode_unicode_references(row['location'])).read()
+
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option('--skip-rebuild-library', action='store_true', default=False)
+    options, args = parser.parse_args()
+    rebuild_library = not options.skip_rebuild_library
+
+    itunes_exporter = iTunesExporter(library_location='~/Music/iTunes/iTunes Music Library.xml', 
+                                     database_location='./library.sq3',
+                                     templates_location=os.path.dirname(os.path.abspath(__file__)),
+                                     rebuild_library=rebuild_library)
+    cherrypy.tree.mount(itunes_exporter, '/', config={'/static': {'tools.staticdir.on': True,
+                                                                  'tools.staticdir.dir': absolute_path('./static')}})
+    cherrypy.quickstart()
---
+++ index.mak
@@ -0,0 +1,116 @@
+<%!
+  import json
+  def js(string):
+    return json.dumps(string).replace("'", "&#39;")
+%>
+
+<html>
+  <head>
+    <title>library :: hithertunes</title>
+
+    <style type="text/css">
+      body {padding:10px; margin:0}
+
+      table {border-collapse:collapse}
+      table tr td {font-family:arial; font-size:10pt; padding-right:5px solid #FFF}
+      table tr:hover {cursor:pointer; background-color:#EEE}
+      table tr.selected {background-color:#B4FFC1}
+
+      div#playlist-header table {margin-top:10px}
+      div#playlist-header tr {background-color: #DDD}
+      div#playlist-header td {font-weight:bold}
+
+      div#jp_playlist_1 {height:30px; overflow:hidden}
+      div#playlist-header {position:fixed; top:0px; height:144px; left:0px; right:0px; overflow:auto; padding:10px 0 0 10px; background-color:#fff}
+      div#playlist-container {padding-top:144px}
+      li#song-name {cursor:pointer}
+    </style>
+
+  </head>
+
+  <body>
+    <div id="jplayer-container"></div>
+
+    <div id="playlist-header">
+      <div class="jp-audio">
+	<div class="jp-type-single">
+	  <div id="jp_interface_1" class="jp-interface">
+	    <ul class="jp-controls">
+	      <li><a href="#" class="jp-play" tabindex="1">play</a></li>
+	      <li><a href="#" class="jp-pause" tabindex="1">pause</a></li>
+	      <li><a href="#" class="jp-stop" tabindex="1">stop</a></li>
+	      <li><a href="#" class="jp-mute" tabindex="1">mute</a></li>
+	      <li><a href="#" class="jp-unmute" tabindex="1">unmute</a></li>
+	    </ul>
+	    <div class="jp-progress">
+	      <div class="jp-seek-bar">
+		<div class="jp-play-bar"></div>
+	      </div>
+	    </div>
+	    <div class="jp-volume-bar">
+	      <div class="jp-volume-bar-value"></div>
+	    </div>
+	    <div class="jp-current-time"></div>
+	    <div class="jp-duration"></div>
+	  </div>
+	  <div id="jp_playlist_1" class="jp-playlist">
+	    <ul>
+	      <li id="song-name">Not Playing</li>
+	    </ul>
+	  </div>
+	</div>
+      </div>
+      
+      <div id="jplayer-inspector"></div>
+      
+      <table>
+	<tr>
+	  <td style="width:300px">artist</td>
+	  <td style="width:334px" colspan="2">track</td>
+	  <td style="width:351px" colspan="2">album</td>
+	</tr>
+      </table>
+    </div>
+
+    <div id="playlist-container">
+    <table>
+    % for song in songs:
+      <tr id="song-row-${song['id']}" onclick='playSong("${song['id']}", ${song['artist'] | js}, ${song['name'] | js})'>
+	<td style="width:300px">${song['artist']}</td>
+	<td style="width:20px">${'%02d' % song['track_number'] if song['track_number'] else ''}</td>
+	<td style="width:312px">${song['name']}</td>
+	<td style="width:34px">${song['year'] if song['year'] else ''}</td>
+	<td style="width:312px">${song['album']}</td>
+      </tr>
+    % endfor
+    </table>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.js"></script>    
+    <script src="/static/jplayer/jquery.jplayer.min.js"></script>
+    <script src="/static/jplayer/jquery.jplayer.inspector.js"></script>
+    <link rel="stylesheet" type="text/css" href="/static/jplayer/jplayer.blue.monday.css">
+    <script>
+      function playSong(songId, artist, name) {
+        $('#jplayer-container').jPlayer('setMedia', {mp3: '/song/' + songId}).jPlayer('play');
+        $("#song-name").text(artist + " - " + name).click(function() { window.location.hash = "song-row-" + songId; });
+
+        // update the table UI to indicate what's playing
+        $("tr.selected").removeClass("selected")
+        $("#song-row-" + songId).addClass("selected")
+      }
+
+      $(function() { 
+          $("#jplayer-container").jPlayer({swfPath: '/static/jplayer',
+                                           solution: 'flash, html',
+                                           supplied: "mp3",
+                                           });
+
+          if (false) {
+            $("#jplayer-inspector").jPlayerInspector({jPlayer: $("#jplayer-container")});
+          }
+
+        });
+    </script>
+  </body>
+</html>
